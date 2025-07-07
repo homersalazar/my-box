2dfc121b6a22ccedf3a91dfa38a0f8f5a5f73c95
Homer Salazar <salazarhomer1@gmail.com>
2025-07-07 17:13:15 +0800

    setup for resources and add navbar

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::prefix('home')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
 });
